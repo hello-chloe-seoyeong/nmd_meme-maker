d5a0ce92f682ac6055cb1a2732b4beeba42d7847
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-11-06 20:45:38 -0800

    Added canvas js - change line width

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const lineWidth = document.getElementById("line-width");
 
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 800;
@@ -7,7 +8,7 @@ const CANVAS_HEIGHT = 800;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 
 let isPaingting = false;
 
@@ -29,7 +30,13 @@ function cancelPainting(event) {
   ctx.beginPath();
 }
 
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
