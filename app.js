b109d395082099fdf8b2fa8a482c7bfd7dc5c87f
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-11-06 21:02:00 -0800

    Added canvas js - mode, reset, eraser btn

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const ctx = canvas.getContext("2d");
 const lineWidth = document.getElementById("line-width");
 const colorInput = document.getElementById("color");
 const colors = Array.from(document.getElementsByClassName("color-option"));
+const modeBtn = document.getElementById("mode-btn");
+const resetBtn = document.getElementById("reset-btn");
+const eraserBtn = document.getElementById("eraser-btn");
 
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 800;
@@ -13,6 +16,7 @@ canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
 
 let isPaingting = false;
+let isFilling = false;
 
 function onMove(event) {
   if (isPaingting) {
@@ -48,13 +52,44 @@ function onColorClick(event) {
   colorInput.value = colorChip;
 }
 
+function onModeChange() {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+}
+
+function onResetCanvas() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserBrush() {
+  ctx.strokeStyle = "white";
+  isFilling = false;
+  modeBtn.innerText = "Fill";
+}
+
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
 
 lineWidth.addEventListener("change", onLineWidthChange);
 colorInput.addEventListener("change", onColorChange);
 colors.forEach((color) => {
   color.addEventListener("click", onColorClick);
 });
+
+modeBtn.addEventListener("click", onModeChange);
+resetBtn.addEventListener("click", onResetCanvas);
+eraserBtn.addEventListener("click", onEraserBrush);
